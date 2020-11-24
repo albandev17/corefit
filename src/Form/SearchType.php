a60e01fdbb0f5aa91728603d508d062c4522c0f1
Alban Moussu <albanmoussu@hotmail.fr>
2020-11-24 13:38:11 +0100

    changement couleur btn filtre

--- src/Form/SearchType.php
+++ src/Form/SearchType.php
@@ -34,7 +34,7 @@ class SearchType extends AbstractType
             ->add('submit', SubmitType::class, [
                 'label'     => 'Filtrer',
                 'attr'      => [
-                    'class' => "btn-block btn-info"
+                    'class' => "btn-block btn-primary"
                 ]
             ])
             ;
